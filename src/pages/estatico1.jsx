30872d04150b062f128f7984f9117f6471eec9ab
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-02-22 08:40:16 -0400

    conteudo estatico

---
+++ src/pages/estatico1.jsx
@@ -0,0 +1,20 @@
+//gerando conteudo de maneira estatica
+//conteudo não depende de propriedade , logo é gerado de forma de estatica
+//um conteudo estatito requer que da mesma forma que aparece do lado do servidor deve aparecer para o ususario
+
+//import { useEffect, useState } from "react"
+
+export default function estatico1() {
+
+    //const [num, setNum]= useState(0)
+
+    //useEffect(() =>{
+     //   setNum(Math.random())
+  //  }, []) 
+
+    return (
+        <div>
+            <h1>Estatico version #2</h1>
+        </div>
+    )
+}
